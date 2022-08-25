f9e84cc9f84d7eb80302f276e19cb51b3f055406
Blake McKenzie <blakemckenzie365@gmail.com>
2022-08-25 22:32:21 +0100

    bookmarks controller routing and methods added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,7 +17,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
     @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
   end
 
   private
