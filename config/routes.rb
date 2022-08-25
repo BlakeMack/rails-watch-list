652f9fb34205f27fa0f765061d217ebb92a1413e
Blake McKenzie <blakemckenzie365@gmail.com>
2022-08-25 15:40:04 +0100

    bookmarks controller and controller#new method added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  # get 'bookmarks/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create]
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
