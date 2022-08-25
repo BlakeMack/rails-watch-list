d1ea6447b0067f2c1faf3f5d7fcc2aa11dc9d591
Blake McKenzie <blakemckenzie365@gmail.com>
2022-08-25 14:43:41 +0100

    lists controller methods added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,10 +4,26 @@ class ListsController < ApplicationController
   end
 
   def show
-    @lists = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def new
     @list = List.new
   end
+
+  def create
+    @list = List.new(review_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:list).permit(:name)
+  end
+
 end
