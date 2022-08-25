d6bcda3c94c723d639aa71c29bdc545c3bbed91d
Blake McKenzie <blakemckenzie365@gmail.com>
2022-08-25 13:03:44 +0100

    listscontroller index and show methods added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'lists', to: 'lists#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  get 'lists/:id', to: 'lists#show'
   # Defines the root path route ("/")
   # root "articles#index"
 end
